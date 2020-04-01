1bf1b0ca89212057863d11972d423cfaac57560c
Gasper Urh <gasper@cytora.com>
2020-04-01 09:05:08 +0200

    added Node.find

--- python/brackettree/tests/test_tree.py
+++ python/brackettree/tests/test_tree.py
@@ -15,19 +15,47 @@ EXAMPLE_INVALID_CASES = read_examples_file('../../../examples/bracket-tree-inval
 
 
 @pytest.mark.parametrize('test_case', EXAMPLE_VALID_CASES)
-def test_parse_valid_bracket_tree(test_case):
+def test_node_parse_valid_bracket_tree(test_case):
     n = parse(test_case['bracketTree'])
     assert n is not None
     assert n.bracket_representation() == test_case['bracketTree']
 
 
 @pytest.mark.parametrize('test_case', EXAMPLE_INVALID_CASES)
-def test_parse_invalid_bracket_tree(test_case):
+def test_node_parse_invalid_bracket_tree(test_case):
     with pytest.raises(ParseException):
         parse(test_case['bracketTree'])
 
 
 @pytest.mark.parametrize('test_case', EXAMPLE_VALID_CASES)
-def test_count_leaves(test_case):
+def test_node_count_leaves(test_case):
     n = parse(test_case['bracketTree'])
     assert n.count_leaves() == test_case['numLeaves']
+
+
+@pytest.mark.parametrize('name_to_find,expected_bracket_string', [
+    ("CD", "CD(Arr(CD))"),
+    ("A", "A(CD(Arr(CD)))(E(F)(G))(CD)(H(D)(MN))"),
+    ("E", "E(F)(G)"),
+    ("MN", "MN"),
+])
+def test_find_existing(name_to_find, expected_bracket_string):
+    bracket_tree = "A(CD(Arr(CD)))(E(F)(G))(CD)(H(D)(MN))"
+    bt = parse(bracket_tree)
+
+    found = bt.find(name_to_find)
+    assert found is not None
+    assert found.bracket_representation() == expected_bracket_string
+
+
+@pytest.mark.parametrize('name_to_find', [
+    "non-existing",
+    "(",
+    ")",
+])
+def test_find_non_existing(name_to_find):
+    bracket_tree = "H(D)(MN)"
+    bt = parse(bracket_tree)
+
+    found = bt.find(name_to_find)
+    assert found is None
--- python/brackettree/tree.py
+++ python/brackettree/tree.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 
 class Node:
@@ -7,8 +7,14 @@ class Node:
         self.name: str = name
         self.children: List[Node] = []
 
-    def find(self, name: str) -> "Node":
-        raise NotImplementedError()
+    def find(self, name: str) -> Optional["Node"]:
+        if self.name == name:
+            return self
+        for c in self.children:
+            r = c.find(name)
+            if r is not None:
+                return r
+        return None
 
     def bracket_representation(self) -> str:
         # avoiding string concatenation since strings are immutable object -
@@ -20,7 +26,7 @@ class Node:
     def count_leaves(self) -> int:
         if len(self.children) == 0:
             return 1
-        return sum([c.count_leaves() for c in self.children])
+        return sum(c.count_leaves() for c in self.children)
 
     def add_child(self, bracket_tree: str) -> None:
         raise NotImplementedError()
